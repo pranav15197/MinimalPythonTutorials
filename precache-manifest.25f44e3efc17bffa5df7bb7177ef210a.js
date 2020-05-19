9a65bad160894853a6184a15a83612e3c9f5e7c8
pranav <pranav.tiwari.eros@gmail.com>
2020-05-19 19:52:02 +0530

    Updates

---
+++ precache-manifest.25f44e3efc17bffa5df7bb7177ef210a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6782144fc212edc728de9a56d9fc4ea4",
+    "url": "/MinimalPythonTutorials/index.html"
+  },
+  {
+    "revision": "8798b28d0dc87c9b554d",
+    "url": "/MinimalPythonTutorials/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2a1dc8c142df441c6c7",
+    "url": "/MinimalPythonTutorials/static/js/2.4c5fa70c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MinimalPythonTutorials/static/js/2.4c5fa70c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8798b28d0dc87c9b554d",
+    "url": "/MinimalPythonTutorials/static/js/main.ada7d9fd.chunk.js"
+  },
+  {
+    "revision": "0547211c79700779e80a",
+    "url": "/MinimalPythonTutorials/static/js/runtime-main.15a800d8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/MinimalPythonTutorials/static/media/logo.5d5d9eef.svg"
+  }
+]);
