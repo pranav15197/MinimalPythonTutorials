0798422c9a2e9b86e8479b3dda45b7f3e86e44cc
pranav <pranav.tiwari.eros@gmail.com>
2020-05-21 06:48:03 +0530

    added analytics

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD6kLBCrS1po-eQiZFl-5gDWFbCSeTQbT8",
+  authDomain: "python-tutorial-44b39.firebaseapp.com",
+  databaseURL: "https://python-tutorial-44b39.firebaseio.com",
+  projectId: "python-tutorial-44b39",
+  storageBucket: "python-tutorial-44b39.appspot.com",
+  messagingSenderId: "603689422938",
+  appId: "1:603689422938:web:1c0c3b3c73ea4646604863",
+  measurementId: "G-RHTFSZ97D3"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
