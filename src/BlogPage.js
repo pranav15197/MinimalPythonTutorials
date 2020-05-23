f31b2ef0c6f18aeea1e3a7ab5dcfde9dce924a1f
pranav <pranav.tiwari.eros@gmail.com>
2020-05-23 09:27:03 +0530

    added discuz

--- src/BlogPage.js
+++ src/BlogPage.js
@@ -1,7 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Disqus from "disqus-react"
+
 
 function BlogPage(props) {
+  const disqusShortname = "python-pranavtiwari-com" //found in your Disqus.com dashboard
+  const disqusConfig = {
+    url: "http://localhost:3000/" + props.component.path,
+    identifier: props.component.path, //this.props.uniqueId
+    title: props.component.title //this.props.title
+  }
   return (
     <div className="container">
       <Link to="/">Home</Link>
@@ -13,6 +21,10 @@ function BlogPage(props) {
           <>Next tutorial -
           <Link to={props.next.path}>{props.next.title}</Link></> : ""}
       </footer>
+      <Disqus.DiscussionEmbed
+        shortname={disqusShortname}
+        config={disqusConfig}
+      />
     </div>
   );
 }
